2831bfe82342e64cb961a5399dbf7176c202147d
sparkHou <1>
2019-11-26 00:34:06 +0800

    #111

---
+++ enableUpdate/index.js
@@ -0,0 +1,12 @@
+$(window).ready(function () {
+
+    /**
+     * 滚动事件
+     */
+    $('body').scrollspy({target: '#navbar-example'})
+    $('.carousel').carousel({
+        pause:"hover"
+    })
+
+
+})

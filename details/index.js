4d7ae1745c1fc6a85e9826f89462c19c201d86df
侯欣榆 <xinyu.hou@dhc.com.cn>
2021-01-23 15:58:59 +0800

    --

--- details/index.js
+++ details/index.js
@@ -3,3 +3,16 @@
  * @date  : 2019-11-26 11:18
  * @Description :
  */
+
+
+
+window.onload = function () {
+  var text = ''
+  for (var i = 0; i < 150; i++) {
+    text += '榆少的小店铺  '
+  }
+  var covers = document.querySelectorAll('.cover')
+  covers.forEach(function (item) {
+    item.innerHTML = text
+  })
+}

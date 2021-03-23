85db97e32d40b7af225824546c3199f3cef8682c
侯欣榆 <xinyu.hou@dhc.com.cn>
2021-03-23 22:04:00 +0800

    重置

--- src/components/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author  sparkHou
- * @date 2021-03-16 22:59
- * @Description:
- */
-import Vue from 'vue';
-import PicList from './PicList.vue';
-
-Vue.component('PicList', PicList);
--- src/store/getters.js
+++
@@ -1,9 +0,0 @@
-/**
- * @author  sparkHou
- * @date 2021-03-14 14:53
- * @Description:
- */
-const getters = {
-  loading: (state) => state.demo.loading,
-};
-export default getters;

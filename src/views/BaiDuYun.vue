ed26b34fbbd327fe191f7444267154291a5eeac4
sparkHou <1>
2021-05-04 22:47:32 +0800

    new

---
+++ src/views/BaiDuYun.vue
@@ -0,0 +1,69 @@
+/**
+* @author  sparkHou
+* @date 2021-05-04 22:35
+* @Description:
+*/
+<template>
+  <div class="BaiDuYun g-container">
+    <div class="g-pageHead">
+      {{title}}
+    </div>
+    <el-tabs v-model="activeName" @tab-click="tabClick">
+      <el-tab-pane label="方法一：开启p2p加速（长期有效）" name="1"/>
+    </el-tabs>
+    <div class="g-infoBox">
+      <div v-if="activeName==='1'">
+        <PicList :list="list1"></PicList>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+const img1 = require('../assets/img/baidu/1.png');
+const img2 = require('../assets/img/baidu/2.png');
+const img3 = require('../assets/img/baidu/3.png');
+const img4 = require('../assets/img/baidu/4.png');
+
+export default {
+  name: 'BaiDuYun',
+  data() {
+    return {
+      activeName: '1',
+      title: '百度云加速教程',
+      list1: [
+        {
+          text: '先把百度云客户端升级到最新版！！！！',
+
+        }, {
+          text: '',
+          url: img1,
+        }, {
+          text: '',
+          url: img2,
+        }, {
+          text: '',
+          url: img3,
+        }, {
+          text: '',
+          url: img4,
+        }, {
+          text: '开启成功',
+        },
+      ],
+    };
+  },
+  mounted() {
+    document.title = this.title;
+  },
+  methods: {
+    tabClick(e) {
+      console.log(e.name);
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped>
+  .BaiDuYun {
+
+  }
+</style>
---
+++ src/views/PythonClassesH.vue
@@ -0,0 +1,51 @@
+/**
+* @author  sparkHou
+* @date 2021-05-04 17:19
+* @Description:
+*/
+<template>
+  <div class="PythonClassesH g-container">
+    <div class="g-pageHead">
+      {{title}}
+    </div>
+    <el-tabs v-model="activeName" @tab-click="tabClick">
+      <el-tab-pane label="1.python开发培训课程" name="1"/>
+    </el-tabs>
+    <div class="g-infoBox">
+      <div v-if="activeName==='1'">
+        <div>视频课件下载地址：
+          <a style="color:blue" target="_blank" href="https://pan.baidu.com/s/14rRkM-U3Y72FG2g9UQ_E3A">
+            链接:点击跳转</a>
+        </div>
+        <div>提取码:trjy</div>
+        <el-divider></el-divider>
+        开发工具下载地址：<a style="color:blue" target="_blank"
+                    href="https://www.jetbrains.com/pycharm/download/#section=windows">点击跳转</a>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'PythonClassesH',
+  data() {
+    return {
+      activeName: '1',
+      title: 'python开发培训课程',
+    };
+  },
+  mounted() {
+    document.title = this.title;
+  },
+  methods: {
+    tabClick(e) {
+      console.log(e.name);
+    },
+  },
+};
+</script>
+<style lang="stylus" scoped>
+  .PythonClassesH {
+
+  }
+</style>
